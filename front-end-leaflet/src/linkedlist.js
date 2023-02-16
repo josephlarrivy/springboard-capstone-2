88a4cca3587f52087093780cb9ff27fb4edae3c2
josephlarrivy <joseph.larrivy@gmail.com>
2023-02-16 16:19:36 -0600

    imported linkedList for this.project

--- front-end-leaflet/src/linkedlist.js
+++ front-end-leaflet/src/linkedlist.js
@@ -1,156 +1,76 @@
-/** Node: node for a singly linked list. */
-
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-    }
+class Item {
+  constructor(event) {
+    this.event = event;
+    this.prev = null;
+    this.next = null;
+  }
 }
-
-/** LinkedList: chained together nodes. */
 
 class LinkedList {
-    constructor(vals = []) {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
+  constructor(event) {
+    this.head = null;
+    this.tail = null;
+    this.prev = null;
+    this.length = 0;
+    this.append(event);
+  }
 
-        for (let val of vals) this.push(val);
+  // push(event): add new value to end of list.
+  append(event) {
+    let newItem = new Item(event);
+
+    if (!this.head) {
+      this.head = newItem;
+      this.tail = this.head;
+      this.prev = null;
+    } else {
+      this.tail.next = newItem;
+      newItem.prev = this.tail
+      this.tail = newItem;
     }
+    this.length += 1;
+  }
 
-    /** _get(idx): retrieve node at idx. */
-    _get(idx) {
-        let cur = this.head;
-        let count = 0;
-
-        while (cur !== null && count != idx) {
-            count += 1;
-            cur = cur.next;
-        }
-
-        return cur;
+  // console.log() all items
+  print() {
+    let current = this.head;
+    while (current !== null) {
+      console.log(current);
+      current = current.next;
     }
+  }
 
-    /** push(val): add new value to end of list. */
-    push(val) {
-        let newNode = new Node(val);
-
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = this.head;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-
-        this.length += 1;
+  // adds new item to front of list
+  unshift(event) {
+    let newItem = new Item(event);
+    if (this.head === null) {
+      this.head = newItem;
+    } else {
+      this.head.prev = newItem;
+      newItem.next = this.head;
+      this.head = newItem;
+      this.head.prev = null;
     }
+    if (this.length === 0) this.tail = this.head;
+    this.length += 1;
+  }
 
-    /** unshift(val): add new value to start of list. */
-    unshift(val) {
-        let newNode = new Node(val);
 
-        if (this.head === null) {
-            this.head = newNode;
-        } else {
-            newNode.next = this.head;
-            this.head = newNode;
-        }
 
-        if (this.length === 0) this.tail = this.head;
-
-        this.length += 1;
-    }
-
-    /** pop(): return & remove last item. */
-    pop() {
-        return this.removeAt(this.length - 1);
-    }
-
-    /** shift(): return & remove first item. */
-    shift() {
-        return this.removeAt(0);
-    }
-
-    /** getAt(idx): get val at idx. */
-    getAt(idx) {
-        if (idx >= this.length || idx < 0) {
-            throw new Error("Invalid index.");
-        }
-
-        return this._get(idx).val;
-    }
-
-    /** setAt(idx, val): set val at idx to val */
-    setAt(idx, val) {
-        if (idx >= this.length || idx < 0) {
-            throw new Error("Invalid index.");
-        }
-
-        let cur = this._get(idx);
-        cur.val = val;
-    }
-
-    /** insertAt(idx, val): add node w/val before idx. */
-    insertAt(idx, val) {
-        if (idx > this.length || idx < 0) {
-            throw new Error("Invalid index.");
-        }
-
-        if (idx === 0) return this.unshift(val);
-        if (idx === this.length) return this.push(val);
-
-        // get the one before it
-        let prev = this._get(idx - 1);
-
-        let newNode = new Node(val);
-        newNode.next = prev.next;
-        prev.next = newNode;
-
-        this.length += 1;
-    }
-
-    /** removeAt(idx): return & remove item at idx, */
-    removeAt(idx) {
-        if (idx >= this.length || idx < 0) {
-            throw new Error("Invalid index.");
-        }
-
-        // special case: remove first item
-
-        if (idx === 0) {
-            let val = this.head.val;
-            this.head = this.head.next;
-            this.length -= 1;
-            if (this.length < 2) this.tail = this.head;
-            return val;
-        }
-
-        let prev = this._get(idx - 1);
-
-        // special case: remove tail
-
-        if (idx === this.length - 1) {
-            let val = prev.next.val;
-            prev.next = null;
-            this.tail = prev;
-            this.length -= 1;
-            return val;
-        }
-
-        // normal case: remove in middle
-
-        let val = prev.next.val;
-        prev.next = prev.next.next;
-        this.length -= 1;
-        return val;
-    }
 
 }
 
+
+
+let list = new LinkedList(1)
+list.append(2)
+list.append(3)
+list.append(4)
+list.append(5)
+list.unshift(6)
+
+
+// console.log(list)
+list.print()
+
 module.exports = LinkedList;
-
-
-let list = new LinkedList([1, 2, 3, 4]);
-
-console.log(list._get(0))
