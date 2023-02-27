cc00daecc6f03e7d6404c6d5f59adde53c5a3112
josephlarrivy <joseph.larrivy@gmail.com>
2023-02-27 17:04:57 -0600

    need to figure out useMap part of leaflet api

--- front-end-leaflet/src/phase-components/ChooseStartLocationForm.js
+++ front-end-leaflet/src/phase-components/ChooseStartLocationForm.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
-import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents } from 'react-leaflet'
 
 import '../css/ChooseStartLocationForm.css'
 
@@ -10,33 +10,57 @@ const ChooseStartLocationForm = ({setStartLocation, setGameState}) => {
 
   const [centerPosition, setCenterPosition] = useState([35, 6])
   const [zoom, setZoom] = useState(2)
-  const [instructions, setInstructions] = useState('on')
+  const [currentZoom, setCurrentZoom] = useState(2)
+  const [instructions, setInstructions] = useState('instructions-1')
 
 
-  const LocationFinder = () => {
-    const map = useMapEvents({
+  const InspectMap = () => {
+    const inspect = new useMap({
       click(e) {
-        let {lat,lng} = e.latlng;
-        let latLonArr = [lat, lng];
-        setStartLocation(latLonArr)
-        setGameState('tutorialPhase')
-      },
-    });
-    return null;
-  };
+        console.log(e)
+      }
+    })
+    return null
+    }
+  
+
+
+
+  // const LocationClickFinder = () => {
+  //   const map = useMapEvents({
+  //     click(e) {
+  //       let {lat,lng} = e.latlng;
+  //       let latLonArr = [lat, lng];
+  //       console.log(e.latlng)
+  //       console.log(currentZoom)
+  //       if (instructions === 'instructions-2' &&
+  //         currentZoom >=11
+  //       ) {
+  //         setStartLocation(latLonArr)
+  //         setGameState('tutorialPhase')
+  //       }
+  //     },
+  //   });
+  //   return null;
+  // };
     
-  const turnInstructionsOff = () => {
-    setInstructions('off')
+  const nextInstructions = () => {
+    if (instructions === 'instructions-1') {
+      setInstructions('instructions-2')
+    } else if (instructions === 'instructions-2') {
+      setGameState('instructions-3')
+    } else if (instructions === 'instructions-3') {
+    }
   }
 
 
-  if (instructions === 'on') {
+  if (instructions === 'instructions-1') {
     return (
       <div>
-        <div className="choose-start-location-instructions">
+        <div className="instructions-1">
           <h1>Instructions</h1>
           <h4>Use the map to select your starting location. Zoom in and click where you would like to defend.</h4>
-          <button onClick={turnInstructionsOff}>Okay</button>
+          <button onClick={nextInstructions}>Okay</button>
         </div>
         <MapContainer
           className="choose-start-location-map"
@@ -48,28 +72,95 @@ const ChooseStartLocationForm = ({setStartLocation, setGameState}) => {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+          <InspectMap />
+          {/* <LocationClickFinder /> */}
         </MapContainer>
       </div>
     )
-  } else if (instructions === 'off') {
-    return (
-      <div>
-        <MapContainer
-          className="choose-start-location-map"
-          key={'mapContainer'}
-          center={centerPosition}
-          zoom={zoom}
-        >
-          <TileLayer key={'tileLayer'}
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
+  } else if (instructions === 'instructions-2') {
+    if (currentZoom <= 2) {
+      return (
+        <div>
+          <div className="instructions-2">
+            <p>Zoom in. Click to select location.</p>
+            {/* <p>Your zoom level: {currentZoom}</p> */}
+            {/* <button onClick={nextInstructions}>Yes</button> */}
+          </div>
+          <MapContainer
+            className="choose-start-location-map"
+            key={'mapContainer'}
+            center={centerPosition}
+            zoom={zoom}
+          >
+            <TileLayer key={'tileLayer'}
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
 
-          <LocationFinder />
-        </MapContainer>
-      </div>
-    )
+            <InspectMap />
+            {/* <LocationClickFinder /> */}
+          </MapContainer>
+        </div>
+      )
+    } else if (currentZoom <= 9) {
+      return (
+        <div>
+          <div className="instructions-2">
+            <p>Please zoom in closer to you desired location.</p>
+            <p>Click to select.</p>
+            {/* <p>Your zoom level: {currentZoom}</p> */}
+            {/* <button onClick={nextInstructions}>Yes</button> */}
+          </div>
+          <MapContainer
+            className="choose-start-location-map"
+            key={'mapContainer'}
+            center={centerPosition}
+            zoom={zoom}
+          >
+            <TileLayer key={'tileLayer'}
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <InspectMap />
+            {/* <LocationClickFinder /> */}
+          </MapContainer>
+        </div>
+      )
+    } else if (currentZoom >= 10) {
+      return (
+        <div>
+          <div className="instructions-2">
+            <p>Choose this location?</p>
+            {/* <p>Your zoom level: {currentZoom}</p> */}
+            <button onClick={nextInstructions}>Yes</button>
+          </div>
+          <MapContainer
+            className="choose-start-location-map"
+            key={'mapContainer'}
+            center={centerPosition}
+            zoom={zoom}
+          >
+            <TileLayer key={'tileLayer'}
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <InspectMap />
+            {/* <LocationClickFinder /> */}
+          </MapContainer>
+        </div>
+      )
+    }
+
+
+
+
+
+
+
   }
+    
 
   
 }
