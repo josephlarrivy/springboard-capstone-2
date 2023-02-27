287ad05f566102029861e30eeafcaf2f184abb0d
josephlarrivy <joseph.larrivy@gmail.com>
2023-02-26 20:06:36 -0600

    able to generate new coordinates

--- front-end-leaflet/src/hooks/useGenerateNewLocation.js
+++ front-end-leaflet/src/hooks/useGenerateNewLocation.js
@@ -16,11 +16,24 @@ const useGenerateNewLocation = () => {
 
   const generateIncident = (center, distance) => {
     let [ x, y ] = center
-    console.log(center)
-    console.log(x)
-    console.log(y)
-    console.log(distance/10)
-    console.log(distance/3)
+    let plusOrMinus1 = Math.random() < 0.5 ? -1 : 1;
+    let plusOrMinus2 = Math.random() < 0.5 ? -1 : 1;
+    let latMoveDist = Math.random()*(distance/10)
+    let lonMoveDist = Math.random()*(distance/3)
+
+    let newLat = x + (plusOrMinus1*latMoveDist)
+    let newlon = y + (plusOrMinus2*lonMoveDist)
+
+    let newCoordinates = [newLat, newlon]
+    
+    console.log(newCoordinates)
+    // console.log(center)
+    // console.log(x)
+    // console.log(y)
+    // console.log(distance/10)
+    // console.log(distance/3)
+
+
   }
 
   return [generateIncident]
